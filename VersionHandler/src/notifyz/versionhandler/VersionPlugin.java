29b30e959d131f992b3e5b83186ee2866618316a
Notifyz <benjamin.dzelle@gmail.com>
2018-04-09 17:44:51 +0200

    1.0-SNAPSHOT

---
+++ VersionHandler/src/notifyz/versionhandler/VersionPlugin.java
@@ -0,0 +1,37 @@
+package notifyz.versionhandler;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import lombok.Getter;
+import lombok.Setter;
+import notifyz.versionhandler.command.ProtocolCommand;
+
+/**
+ *
+ * @author Notifyz
+ * This is the main class for VersionHandler.
+ */
+
+@Getter
+@Setter
+public class VersionPlugin extends JavaPlugin {
+
+	public VersionPlugin instance;
+
+	/**
+	 * @param Enabling plugin.
+	 */
+	@Override
+	public void onEnable() {
+		instance = this;
+		getCommand("protocol").setExecutor(new ProtocolCommand());
+	}
+
+	/**
+	 * @param Disabling plugin. (This is completely unnecessary but I will keep it)
+	 */
+	@Override
+	public void onDisable() {
+		instance = null;
+	}
+}
